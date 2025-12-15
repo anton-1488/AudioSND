a9da7bc435a2fcff3f34569fde0a59c96a497960
anton-1488 <ap67253100@gmail.com>
2025-12-15 12:21:06 +0300

    Audio device update

--- core/src/main/java/org/plovdev/audioengine/devices/OutputAudioDevice.java
+++ core/src/main/java/org/plovdev/audioengine/devices/OutputAudioDevice.java
@@ -8,6 +8,5 @@ import java.nio.ByteBuffer;
  */
 public interface OutputAudioDevice extends AudioDevice {
     int write(ByteBuffer byteBuffer);
-    int write(ByteBuffer byteBuffer, int start, int end);
     void flush();
 }
