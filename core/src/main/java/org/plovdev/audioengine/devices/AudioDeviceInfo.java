70b779752467be9586a85e7be7f992fc4bca1101
anton-1488 <ap67253100@gmail.com>
2025-12-15 12:31:13 +0300

    AD Fix

--- core/src/main/java/org/plovdev/audioengine/devices/AudioDeviceInfo.java
+++ core/src/main/java/org/plovdev/audioengine/devices/AudioDeviceInfo.java
@@ -4,5 +4,4 @@ import org.plovdev.audioengine.tracks.format.TrackFormat;
 
 import java.util.Set;
 
-public record AudioDeviceInfo(String id, String name, String vendor, String version, int channels, Set<TrackFormat> supportedForamts) {
-}
+public record AudioDeviceInfo(String id, String name, String vendor, String version, int channels, Set<TrackFormat> supportedForamts) {}
